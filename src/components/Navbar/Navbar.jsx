1bdd2491760ce0e9d63bee8eb293e2bd511f4fa5
Gem <mojeedibrahim13@gmail.com>
2026-06-07 00:11:55 -0700

    adding and styling the navs

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,12 +3,26 @@ import React from 'react'
 const Navbar = () => {
   return (
     <div>
-      <ul>
-        <li>Home</li>
-        <li>About</li>
-        <li>Contact</li>
-        <li>Blog</li>
-      </ul>
+      <div className='flex hidden justify-between items-center h-16 bg-blue-500 mx-auto px-6 text-white'>
+        <h1>EATGO</h1>
+        <ul className='flex'>
+          <li className='p-4'>Home</li>
+          <li className='p-4'>About</li>
+          <li className='p-4'>Contact</li>
+          <li className='p-4'>Blog</li>
+        </ul>
+      </div>
+      <div>
+        {/* icon */}
+      </div>
+      <div className='fixed left-0 top-0 border-r h-full bg-[#bd2] w-[50%]'>
+        <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Blog</li>
+        </ul>
+      </div>
     </div>
   )
 }
