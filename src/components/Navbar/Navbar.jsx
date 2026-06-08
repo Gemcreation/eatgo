d72808d2159928d1ee0f5ceeff86569df7a0caa6
Gem <mojeedibrahim13@gmail.com>
2026-06-08 04:20:41 -0700

    all style in nav done

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
     <div>
       <div className='flex justify-between items-center h-16 bg-blue-500 mx-auto px-6 text-white'>
         <h1>EATGO</h1>
-        <ul className='flex'>
+        <ul className='hidden md:flex'>
           <li className='p-4'>Home</li>
           <li className='p-4'>About</li>
           <li className='p-4'>Contact</li>
@@ -29,8 +29,8 @@ const Navbar = () => {
       </div> note: hidden works same as = left-[-100%]
       */}
       <div className={!nav ? "fixed left-0 top-0 border-r h-full bg-[#bd2] w-[50%]" : "fixed top-0 -translate-x-full transition-all duration-600 ease-in-out "}>
-        <h1 className="m-4">EATGO</h1>
-        <ul className="uppercase">
+        {/* <h1 className="m-4">EATGO</h1> */}
+        <ul className="uppercase mt-2">
           <li className="p-4 border-b border-gray-500">Home</li>
           <li className="p-4 border-b border-gray-500">About</li>
           <li className="p-4 border-b border-gray-500">Contact</li>
