2d435e84617eac8aa39e4cb2aee593034a4362cb
Gem <mojeedibrahim13@gmail.com>
2026-06-08 04:17:37 -0700

    commitng the nav style

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,7 +1,12 @@
 
+import { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
+  const [nav, setNav] = useState(false);
+  const handleNav = () => {
+    setNav(!nav)
+  }
   return (
     <div>
       <div className='flex justify-between items-center h-16 bg-blue-500 mx-auto px-6 text-white'>
@@ -11,18 +16,25 @@ const Navbar = () => {
           <li className='p-4'>About</li>
           <li className='p-4'>Contact</li>
           <li className='p-4'>Blog</li>
-        </ul>
-      </div>
-      <div>
+        </ul>   
+        
         {/* icon */}
-        <AiOutlineMenu />
+      <div onClick={handleNav} className="block md:hidden">
+        {!nav ? 
+        <AiOutlineClose size={20}/> : <AiOutlineMenu size={20}/>}
       </div>
-      <div className='fixed left-0 top-0 border-r h-full bg-[#bd2] w-[50%]'>
-        <ul>
-          <li>Home</li>
-          <li>About</li>
-          <li>Contact</li>
-          <li>Blog</li>
+      {/* for mobile icons */}
+      </div>
+      {/* <div className={nav ? "fixed left-0 top-0 border-r h-full bg-[#bd2] w-[50%]" : "fixed hidden"}>
+      </div> note: hidden works same as = left-[-100%]
+      */}
+      <div className={!nav ? "fixed left-0 top-0 border-r h-full bg-[#bd2] w-[50%]" : "fixed top-0 -translate-x-full transition-all duration-600 ease-in-out "}>
+        <h1 className="m-4">EATGO</h1>
+        <ul className="uppercase">
+          <li className="p-4 border-b border-gray-500">Home</li>
+          <li className="p-4 border-b border-gray-500">About</li>
+          <li className="p-4 border-b border-gray-500">Contact</li>
+          <li className="p-4">Blog</li>
         </ul>
       </div>
     </div>
